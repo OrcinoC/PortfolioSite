530400d1ae6c4306d2a8574cd1af0948e380d112
Colby Orcino <colbyorcino@Colbys-Air.lan>
2022-05-15 20:05:23 -0400

    First commit

---
+++ regexCalcPort/scriptTest.js
@@ -0,0 +1,49 @@
+const resetText = ""
+let min = 0;
+let max = 0;
+
+function resetRegex() {
+    document.getElementById('regexResult').innerHTML = resetText;
+};
+
+function validate() {
+    min = document.getElementById("minimum").value;
+    max = document.getElementById("maximum").value;
+
+    if (max - min > 0) {
+        if (min >= 1 && min < 10) {
+            if (max >= 1 && max <= 100) {
+                calculate();
+            } else {
+                document.getElementById('regexResult').innerHTML = "Maximum must be between 1 and 100.";
+            }
+        } else {
+            document.getElementById('regexResult').innerHTML = "Minimum must be between 1 and 9.";
+        }
+    } else {
+        document.getElementById('regexResult').innerHTML = "Minimum must be less than maximum.";
+    }   
+};
+
+function calculate() {
+    min = document.getElementById("minimum").value;
+    max = document.getElementById("maximum").value;
+
+    let numReq = "(?=.*\\d)";
+    let specReq = "(?=.*[@$!%*?&])";
+    let capitalReq = "(?=.*[A-Z])";
+    const regexArr = ["^", "[A-Za-z\\d@$!%*?&]{" + min + "," + max + "}$"]
+
+    if(document.getElementById("numberReq").checked) { //Number Required
+        regexArr.splice(1, 0, numReq); 
+        document.getElementById('regexResult').innerHTML = regexArr.join("");
+    }
+    if (document.getElementById("capitalLetter").checked) { //Capital Required
+        regexArr.splice(1, 0, capitalReq);
+        document.getElementById('regexResult').innerHTML = regexArr.join("");
+    } 
+    if (document.getElementById("specialChar").checked) { //Special Char Required
+        regexArr.splice(1, 0, specReq);
+        document.getElementById('regexResult').innerHTML = regexArr.join("");               
+    }
+};
